75f5ab2cf1f3a5382436253172c303569cc3fe1b
Kwan Suppaiboonsuk <kwan@Kwans-MacBook-Pro-2.local>
2024-06-15 15:31:02 +0200

    app files

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const router = express.Router();
 
@@ -9,3 +11,21 @@ router.get("/", (req, res) => {
 
 app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
+
+// middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// routing
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
